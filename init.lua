3825b63be715d8dbf9946f6efc3f97113de5e32f
RupenderSinghRathore <rupender.6616403224@std.ggsipu.ac.in>
2025-05-13 20:48:41 +0530

    ColorTheme Modern Dark

--- init.lua
+++ init.lua
@@ -3,32 +3,32 @@ require("core.keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error("Error cloning lazy.nvim:\n" .. out)
+    end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- plugin's go here
-  require("plugins.colortheme"),
-  require("plugins.neotree"),
-  require("plugins.treesitter"),
-  require("plugins.telescope"),
-  require("plugins.lsp"),
-  require("plugins.autocompletion"),
-  -- require("plugins.autoformatting"),
-  require("plugins.indent-blankline"),
-  require("plugins.comment"),
-  require("plugins.auto-tag"),
-  require("plugins.misc"),
-  require("plugins.alpha"),
-  require("plugins.none-ls"),
-  require("plugins.debugger"),
-  -- require("plugins.noice"),
-  require("plugins.codium"),
-  require("plugins.snippets"),
-  require("plugins.dev"),
+    -- plugin's go here
+    require("plugins.colortheme"),
+    require("plugins.neotree"),
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.lsp"),
+    require("plugins.autocompletion"),
+    -- require("plugins.autoformatting"),
+    require("plugins.indent-blankline"),
+    require("plugins.comment"),
+    require("plugins.auto-tag"),
+    require("plugins.misc"),
+    require("plugins.alpha"),
+    require("plugins.none-ls"),
+    require("plugins.debugger"),
+    -- require("plugins.noice"),
+    require("plugins.codium"),
+    require("plugins.snippets"),
+    require("plugins.dev"),
 })
--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,46 +1,52 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = false, -- Load immediately
-  --   priority = 100, -- Load before other plugins
-  --   config = function()
-  --     require("tokyonight").setup({
-  --       style = "night",
-  --       transparent = false, -- Default is not transparent
-  --       terminal_colors = true,
-  --     })
-  --     vim.cmd.colorscheme("tokyonight")
-  --
-  --     -- Function to toggle transparency
-  --     local function toggle_transparency()
-  --       local current_transparency = require("tokyonight.config").options.transparent
-  --       require("tokyonight").setup({
-  --         transparent = not current_transparency,
-  --         style = "night",
-  --       })
-  --       vim.cmd.colorscheme("tokyonight") -- Reload colorscheme
-  --     end
-  --
-  --     -- Set keybinding to <leader>tt (change if you prefer another keybinding)
-  --     vim.keymap.set("n", "<leader>tt", toggle_transparency, { noremap = true, silent = true })
-  --   end,
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- Load immediately
+    priority = 100, -- Load before other plugins
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        transparent = false, -- Default is not transparent
+        terminal_colors = true,
+        on_colors = function(colors)
+          colors.bg = "#16161E" -- Your desired darker hex color
+          -- Specifically for 'night' style's dark background if it's different
+          colors.bg_dark = "#16161E" -- Your desired darker hex color
+        end,
+      })
+
+      vim.cmd.colorscheme("tokyonight")
+
+      -- Function to toggle transparency
+      local function toggle_transparency()
+        local current_transparency = require("tokyonight.config").options.transparent
+        require("tokyonight").setup({
+          transparent = not current_transparency,
+          style = "night",
+        })
+        vim.cmd.colorscheme("tokyonight") -- Reload colorscheme
+      end
+
+      -- Set keybinding to <leader>tt (change if you prefer another keybinding)
+      vim.keymap.set("n", "<leader>tt", toggle_transparency, { noremap = true, silent = true })
+    end,
+  },
   -- { "catppuccin/nvim", name = "catppuccin", priority = 100 },
   --
   --
 
   -- ### Rose - pine theme
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 100,
-    config = function()
-      -- require("rose-pine").setup({
-      -- disable_background = false, -- Remove background
-      --   disable_float_background = true, -- Transparent floats
-      -- })
-      vim.cmd("colorscheme rose-pine-moon")
-      -- vim.cmd("colorscheme rose-pine")
-    end,
-  },
+  -- {
+  --     "rose-pine/neovim",
+  --     name = "rose-pine",
+  --     priority = 100,
+  --     config = function()
+  --         -- require("rose-pine").setup({
+  --         -- disable_background = false, -- Remove background
+  --         --   disable_float_background = true, -- Transparent floats
+  --         -- })
+  --         vim.cmd("colorscheme rose-pine-moon")
+  --         -- vim.cmd("colorscheme rose-pine")
+  --     end,
+  -- },
 }
